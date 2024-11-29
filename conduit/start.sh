a9441f1a5a9a67b0cb3b337485381e68561bdb82
Donald Hermes <Donald.Hermes@spirit-testing.com>
2024-11-29 13:19:32 +0100

    Use multistage buildings

--- conduit/start.sh
+++ conduit/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python backend/manage.py runserver 0.0.0.0:8000 &
+/home/spirit/app/backend/.venv/bin/python backend/manage.py runserver 0.0.0.0:8000 &
 npm --prefix=frontend start &
 wait -n
 exit $?
